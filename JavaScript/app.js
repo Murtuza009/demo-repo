3fffb81718b5658bfd058d6aa3e3ab53bc55f53e
Murtuza <murtuzaprograming@gmail.com>
2023-07-25 15:54:57 +0500

    Compelet css & work javascript

---
+++ JavaScript/app.js
@@ -0,0 +1,5 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const currentTime = new Date();
+console.log(currentTime.getHours())
