db25650b5e79328ff87285784b44787f7129e170
Murtuza <murtuzaprograming@gmail.com>
2023-07-26 12:42:36 +0500

    compeleted

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,12 @@
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
-const currentTime = new Date();
-console.log(currentTime.getHours())
+// const currentTime = new Date();
+// console.log(currentTime.getSeconds());
+
+setInterval(() => {
+    const currentTime = new Date();
+    hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    minutes.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    seconds.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000);
